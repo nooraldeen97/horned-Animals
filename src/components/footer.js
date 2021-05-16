f3490f27d245a1e1561d5c76308204a95bdc87bd
nooraldeen97 <noor-aldeen97@hotmail.com>
2021-05-16 22:22:12 +0300

    finished50%-lab-1

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class footer extends React.Component{
+
+    render(){
+        return(
+            
+            <p>author name : Nooraldeen khalaf</p>
+        )
+    }
+
+}
+
+
+export default footer;
